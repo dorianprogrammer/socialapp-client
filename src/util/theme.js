999dae12db83b1046c2a7a8214269cd27270f90f
dorianprogrammer <dorianrr98@gmail.com>
2021-05-30 09:50:10 -0600

    Initial commit

---
+++ src/util/theme.js
@@ -0,0 +1,39 @@
+export default {
+  palette: {
+    primary: {
+      main: "#880e4f",
+    },
+    secondary: {
+      main: "#ad1457",
+    },
+  },
+  spreadThis: {
+    typography: {
+      userNextVariants: true,
+    },
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      margin: "20px auto 20px auto",
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto",
+    },
+    textField: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
